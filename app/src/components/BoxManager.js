d4fb8f93e43f83b929e1c530902d0b3aeae1ee13
Valentin Hobusch <valentinhobusch+lw@gmail.com>
2021-10-12 12:45:55 +0200

    cardlist

--- app/src/components/BoxManager.js
+++ app/src/components/BoxManager.js
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react"
 import { APIgetBox } from "../api/API";
 import { SlotForm } from "./SlotForm";
+import { GoArrowBoth } from "react-icons/go";
+import { colors } from "../colors"
 
 export function BoxManager() {
   let {id} = useParams()
@@ -29,6 +31,18 @@ export function BoxManager() {
             <div key={`slot-${slot.order}`} className="slot-list-slot">
               <div>Level: {slot.order}</div>
               <SlotForm slot={slot} />
+            </div>
+          )
+        })}
+      </div>
+      <h3>Cards:</h3>
+      <div className="cards-list">
+        {box.cards.map((card) => {
+          return (
+            <div key={`card-${card.id}`} className="card-list-card" style={{backgroundColor: colors[card.color]}}>
+              <p>{card.from}</p>
+              <GoArrowBoth />
+              <p>{card.to}</p>
             </div>
           )
         })}
